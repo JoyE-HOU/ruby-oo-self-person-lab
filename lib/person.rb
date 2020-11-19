93af55d4fbaaac2384c3368d7f2447ebb5f3b6fa
Joy Evans <joyevans@me.com>
2020-11-18 18:01:45 -0600

    commit tree faadc3b4070edc9ced433be9a7a88953fb1837ef

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_writer :bank_account
+
+    def initialize(name)
+        @name = name
+    end
+    
+    def bank_account
+        bank_account = 25
+    end
+
+    def happiness
+        happiness = 8
+        #happiness += 1
+    end
+end
+
+stella = Person.new("Stella")
